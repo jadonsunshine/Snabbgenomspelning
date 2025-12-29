e4bdb67d4c0a9de0cb449e80f00fc5bb3b8ef13f
Jadonamite <jadonamite@gmail.com>
2025-12-29 17:30:57 +0100

    Contract Test using node

--- tests/onchain-notepad.test.ts
+++ tests/onchain-notepad.test.ts
@@ -1,21 +1,15 @@
-
-import { describe, expect, it } from "vitest";
+import { describe, it, expect } from 'vitest';
+import { Cl } from '@stacks/transactions';
 
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+const wallet1 = accounts.get("wallet_1")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+describe('Onchain Notepad', () => {
+  it('writes and reads a note', () => {
+    const write = simnet.callPublicFn('onchain-notepad', 'update-note', [Cl.stringAscii("Secret Note")], wallet1);
+    expect(write.result).toBeOk(Cl.bool(true));
 
-describe("example tests", () => {
-  it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+    const read = simnet.callReadOnlyFn('onchain-notepad', 'get-note', [Cl.standardPrincipal(wallet1)], wallet1);
+    expect(read.result).toBeOk(Cl.some(Cl.stringAscii("Secret Note")));
   });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
 });
